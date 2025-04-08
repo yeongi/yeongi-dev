46ef2491a9015260c409681a79debefc7cb54e0d
yeongi <cbg5255@gmail.com>
2025-04-08 15:38:27 +0900

    feat: integrate Preact for enhanced performance and add Greeting component with random messages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-	site: "https://yeongi-dev.netlify.app/",
+  site: "https://yeongi-dev.netlify.app/",
+  integrations: [preact()],
 });
