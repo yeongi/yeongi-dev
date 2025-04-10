fe00523460eb979dfe26006afc56541870c446f4
yeongi <cbg5255@gmail.com>
2025-04-10 16:24:42 +0900

    feat : yeongi 블로그로 변경

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
 	const posts = await getCollection("blog");
 	return rss({
-		title: "Astro Learner | Blog",
-		description: "My journey learning Astro",
+		title: "Yeongi's Dev | Blog",
+		description: "This is Yeongi's Dev Blog",
 		site: context.site,
 		items: posts.map((post) => ({
 			title: post.data.title,
